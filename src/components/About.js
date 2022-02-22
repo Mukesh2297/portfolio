677b19d6a544b30115d809813047cbbcaa32a156
Mukesh.R1 <Mukesh.R1@contractor.tatacommunications.com>
2022-02-22 18:18:46 +0530

    adding header

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import myImg from "../assets/my-img-2.jpg";
+
+const About = () => {
+    return (
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-lg-6 B">
+                    <div class="card card-inverse card-primary">
+                        <img height="10%"
+                            src={myImg}
+                            class="img-fluid"
+                            alt="Profile Picture"
+                        /></div>
+                </div>
+                <div class="col-md-4 col-lg-3 G">
+                    <div class="row h-100">
+                        <div class="col-md-6 col-lg-6 B h-50 pb-3">
+                            <div class="card card-inverse card-success h-100"></div>
+                        </div>
+                        <div class="col-md-6 col-lg-6 B h-50 pb-3">
+                            <div class="card card-inverse bg-success h-100"></div>
+                        </div>
+                        <div class="col-md-12 h-50">
+                            <div class="card card-inverse bg-danger h-100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import {Nav,Navbar} from "react-bootstrap";
+
+const Header = () => {
+  let headerContents = ["ABOUT", "MY WORK", "CONTACT", "SOCIAL"];
+
+  return (
+      <Navbar bg='dark' variant='dark' expand='sm' collapseOnSelect> 
+      <div className="container">
+      <Navbar.Brand>
+            Mukesh
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+        <Nav className="ms-auto">
+            <Nav.Link href="#about">ABOUT</Nav.Link>
+            <Nav.Link >WORK</Nav.Link>
+            <Nav.Link >CONTACT</Nav.Link>
+            <Nav.Link >SOCIAL</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </div>  
+      </Navbar>
+  );
+};
+
+export default Header;
